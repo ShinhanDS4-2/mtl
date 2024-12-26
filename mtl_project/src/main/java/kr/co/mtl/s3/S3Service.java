86b6152546ebe591e2120773618f0f72c6bffb55
Hyeon-Uk-Kang <jaron0920@naver.com>
2024-12-26 15:59:33 +0900

    s3Service 이미지 업로드

--- mtl_project/src/main/java/kr/co/mtl/s3/S3Service.java
+++ mtl_project/src/main/java/kr/co/mtl/s3/S3Service.java
@@ -1,13 +1,13 @@
 package kr.co.mtl.s3;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
@@ -20,24 +20,20 @@ public class S3Service {
 	@Value("${aws.s3.bucket}")
 	private String BUCKET;
 	
-	/**
-	 * 파일 업로드
-	 * @param mFile
-	 * @param filePath
-	 * @return
-	 * @throws Exception
-	 */
+	@Value("${aws.s3.region}")
+	private String region;
+	
+
 	public String uploadFile(MultipartFile mFile,String filePath) throws IOException{
-		
-			InputStream inputStream = mFile.getInputStream();
-			
-			PutObjectRequest putObjectRequest=PutObjectRequest.builder()
-					.bucket(BUCKET)
-					.key(filePath)
-					.build();
-			
-			s3Client.putObject(putObjectRequest,software.amazon.awssdk.core.sync.RequestBody.fromInputStream(inputStream, mFile.getSize()));
-			
-			return "File uploaded successfully to s3 bucket: "+BUCKET+" with filename: " + mFile.getOriginalFilename();
+	  
+	  PutObjectRequest putObjectRequest=PutObjectRequest.builder() 
+			  .bucket(BUCKET)
+			  .key(filePath) .build();
+	  
+	  s3Client.putObject(putObjectRequest,RequestBody.fromInputStream(mFile.getInputStream(), mFile.getSize()));
+	  
+	  // S3 객체 URL 반환
+      return String.format("https://%s.s3.%s.amazonaws.com/%s", BUCKET, region, filePath);
 	}
+	 
 }
