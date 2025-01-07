7e2d43728d0be22f12a45c649e49e96fb4d62d8b
sangwooklee <145044337+sangwookle@users.noreply.github.com>
2025-01-07 15:47:01 +0900

    공지사항 페이징 처리

--- mtl_project/src/main/java/kr/co/mtl/notice/NoticeMapper.java
+++ mtl_project/src/main/java/kr/co/mtl/notice/NoticeMapper.java
@@ -1,15 +1,28 @@
 package kr.co.mtl.notice;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface NoticeMapper {
 
-    // 페이지 처리된 Notice 목록 조회
-    List<Notice> selectNoticesWithPaging(int limit, int offset);
+    // Notice 목록 가져오기
+    List<Notice> selectNoticesWithPaging(@Param("limit") int limit, @Param("offset") int offset);
 
-    // 전체 Notice 개수 조회
+    // 전체 Notice 개수 가져오기
     int selectTotalNoticeCount();
+
+    // 이용안내 등록
+    void insertNotice(Map<String, Object> param);
+
+    // 이용안내 수정
+    void updateNotice(Map<String, Object> param);
+
+    // 이용안내 삭제
+    void deleteNotice(Map<String, Object> param);
+
+    // 이용안내 리스트 가져오기
+    Map<String, Object> getNoticeList(Map<String, Object> param);
 }
--- mtl_project/src/main/java/kr/co/mtl/notice/NoticeService.java
+++ mtl_project/src/main/java/kr/co/mtl/notice/NoticeService.java
@@ -1,9 +1,9 @@
 package kr.co.mtl.notice;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
-
 @Service
 public class NoticeService {
 
@@ -21,5 +21,28 @@ public class NoticeService {
     // 전체 Notice 개수 가져오기
     public int getTotalNoticeCount() {
         return noticeMapper.selectTotalNoticeCount();
+    }
+
+    // 이용안내 등록
+    public Map<String, Object> insertNotice(Map<String, Object> param) {
+        noticeMapper.insertNotice(param);
+        return param;
+    }
+
+    // 이용안내 수정
+    public Map<String, Object> updateNotice(Map<String, Object> param) {
+        noticeMapper.updateNotice(param);
+        return param;
+    }
+
+    // 이용안내 삭제
+    public Map<String, Object> deleteNotice(Map<String, Object> param) {
+        noticeMapper.deleteNotice(param);
+        return param;
+    }
+
+    // 이용안내 리스트
+    public Map<String, Object> getNoticeList(Map<String, Object> param) {
+        return noticeMapper.getNoticeList(param);
     }
 }
